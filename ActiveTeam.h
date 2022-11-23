582c76bde0147bca6da3229e41baf6ac04c9de96
duri12 <eyalgor102@gmail.com>
2022-11-23 20:23:37 +0200

    added the Player Team and ActiveTeam classes

---
+++ ActiveTeam.h
@@ -0,0 +1,11 @@
+#ifndef DATA_STRUCT_EX1_ACTIVETEAM_H
+#define DATA_STRUCT_EX1_ACTIVETEAM_H
+
+#include "Team.h"
+class ActiveTeam {
+private:
+    Team *team;// the team pointer
+    int score;// the score the team has in the tournament
+};
+
+#endif //DATA_STRUCT_EX1_ACTIVETEAM_H
